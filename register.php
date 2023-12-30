a90db7d863eac796b08696179a67e747cd0bf99f
Rodney St. Cloud <hoyrod1@aol.com>
2023-12-30 00:41:50 -0500

    Updated The Rester Page
    All the code in the "register.php" file has been updated.
    
    The input field data is now validated.
    
    If there is errors in the form data all the errors are stored in an array and variables.
    
    The array and error variable is used to capture what errors have been triggered.
    
    If the error variable and array has a error value the specific error will be printed to the page using the set error variable.
    
    For image field the "$_FILES" SUPERGLOBAL is used to determine if there is a image to uploade.

--- register.php
+++ register.php
@@ -10,67 +10,258 @@
  * @license  STC Media inc
  * @link     https://cms/registration.php
  */
+
 require_once "includes/session.php";
 require_once "includes/db_conn.php";
 require_once "includes/functions.php";
 require_once "includes/date_time.php";
 
+// define $_POST variables and set to empty values
+$name         = "";
+$email        = "";
+$contact_num  = "";
+$username     = "";
+$password     = "";
+$con_password = "";
+$phot_err     = "";
 
-if (isset($_POST['submit'])) {
+// define error variables and set to empty values
+$nameErr     = "";
+$emailErr    = "";
+$contactErr  = "";
+$usernameErr = "";
+$passwordErr = "";
+$confpassErr = "";
+$passNoMatchErr = "";
 
-    if (!empty($_POST['name'])
-        && !empty($_POST['email']) 
-        && !empty($_POST['contact_num']) 
-        && !empty($_POST['username']) 
-        && !empty($_POST['password'])
-    ) { 
+// define form data and image error array and set to empty values
+$formDataErr = [];
+$photoErr    = [];
 
+//- CODE TO REGISTER USER IF THE REQUEST METHOD IS POST $_POST["submit"] ISSET -//
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 
-        $name            = testInput($_POST['name']);
-        $email           = testInput($_POST['email']);
-        $contact_num     = testInput($_POST['contact_num']);
-        $username        = testInput($_POST['username']);
-        $password        = testInput($_POST['password']);
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        // ADD SECURITY FOR UPLOADED IMAGES
-        $target_dir      = "uploads/";
-        $photo           = $_FILES['photo']['name'];
-        $target_file     = $target_dir.basename($photo);
-        $uploadOk        = 1;
-        $imageFileType   = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        $test_db = new Database("localhost", "root", "root", "cms");
-
-        $sql = "INSERT INTO user_record(
-					name, 
-					email, 
-					contact_num, 
-					username, 
-					password, 
-					photo)
-					VALUES(:namE, :emaiL, :contact_nuM, :usernamE, :passworD, :photO)";
-
-        $prepare_stmt = $test_db->conn()->prepare($sql);
-        $prepare_stmt->bindValue(':namE', $name);
-        $prepare_stmt->bindValue(':emaiL', $email);
-        $prepare_stmt->bindValue(':contact_nuM', $contact_num);
-        $prepare_stmt->bindValue(':usernamE', $username);
-        $prepare_stmt->bindValue(':passworD', $hashed_password);
-        $prepare_stmt->bindValue(':photO', $photo);
-        $execute = $prepare_stmt->execute();
-        move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
-
-        if ($execute) {
-            $test_db = null;
-            $_SESSION["success_message"] = 'You have registered, please login.';
-            redirect('login.php');
-
-        } else {
-            echo '<span class="error">Record has not been submitted!"</span>' ;
-        }
+    //------------ CODE TO VALIDATE IF THE FORM DATA IS VALID ------------//
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+        array_push($formDataErr, $nameErr);
     } else {
-        echo '<span class="error">Please enter all fields!</span>';
+        $name = testInput($_POST['name']);
+        if (!preg_match("/^[a-zA-Z-'\. ]*$/", $name)) {
+            $nameErr = "Only letters hyphens, spaces and periods";
+            array_push($formDataErr, $nameErr);
+        }
     }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+        array_push($formDataErr, $emailErr);
+    } else {
+        $email = testInput($_POST['email']);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            array_push($formDataErr, $emailErr);
+        }
+    }
+
+    if (empty($_POST["contact_num"])) {
+        $contactErr = "Contact Number required";
+        array_push($formDataErr, $contactErr);
+    } else {
+        $contact_num = testInput($_POST['contact_num']);
+        if (!preg_match("/[0-9]/", $contact_num)) {
+            $contactErr = "Only numbers and hyphens";
+            array_push($formDataErr, $contactErr);
+        }
+    }
+
+    if (empty($_POST["username"])) {
+        $usernameErr = "Username required";
+        array_push($formDataErr, $usernameErr);
+    } else {
+        $username = testInput($_POST['username']);
+        if (!preg_match("/^[0-9A-Za-z]{6,16}$/", $username)) {
+            $usernameErr = "Please enter between 6-16 letters and numbers";
+            array_push($formDataErr, $usernameErr);
+        }
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password required";
+        array_push($formDataErr, $passwordErr);
+    } else {
+        $password = testInput($_POST['password']);
+        // find one number
+        // one uppercase letter
+        // one lowercase letter
+        // and one character that is NOT alphanumeric
+        // a string with any characters between 6 and 32 characters long
+        // /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^0-9A-Za-z]).{6,32}$/
+        if (!preg_match("/^(?=.*?[0-9])(?=.*?[A-Za-z]).{6,32}$/", $password)) {
+            $passwordErr = "Invalid password";
+            array_push($formDataErr, $passwordErr);
+        }
+    }
+
+    if (empty($_POST["confirm_pass"])) {
+        $confpassErr = "Confirm Password required";
+        array_push($formDataErr, $confpassErr);
+    } else {
+        $con_password = testInput($_POST['confirm_pass']);
+    }
+
+    if ($password !== $con_password) {
+        $passNoMatchErr = "Password doesn't match";
+        array_push($formDataErr, $passNoMatchErr);
+    } else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    }
+    // --------------------------------------------------------------------------- //
+    if (empty($formDataErr)) {
+
+        if ($_FILES["photo"]["size"] === 0) {
+
+            // ---CODE TO INSERT VALIDATED FORM DATA TO DATABASE IF NO IMAGE -- //
+            $database      = new Database("localhost", "root", "root", "cms");
+        
+            $sql           = "INSERT INTO user_record(name, 
+                                                        email, 
+                                                        contact_num, 
+                                                        username, 
+                                                        password)
+                                VALUES(:namE, 
+                                        :emaiL, 
+                                        :contact_nuM, 
+                                        :usernamE, 
+                                        :passworD)";
+        
+            $prepare_stmt  = $database->conn()->prepare($sql);
+            $prepare_stmt->bindValue(':namE', $name);
+            $prepare_stmt->bindValue(':emaiL', $email);
+            $prepare_stmt->bindValue(':contact_nuM', $contact_num);
+            $prepare_stmt->bindValue(':usernamE', $username);
+            $prepare_stmt->bindValue(':passworD', $hashed_password);
+            $execute = $prepare_stmt->execute();
+        
+            if ($execute) {
+                $test_db = null;
+                $_SESSION["success_message"] = "You have registered, please login";
+                redirect("login.php");
+        
+            } else {
+                $test_db = null;
+                $_SESSION["error_message"] = "Record has not been submitted";
+                // echo '<span class="error">Record has not been submitted!"</span>';
+            }
+            // ------------------------------------------------------------------- //
+        } else {
+
+            // ------ CODE TO UPLOAD IMAGE IF $_FILES["photo"]["size"] ISSET ----- //
+            $target_dir    = "uploads/";
+            $photo         = $_FILES['photo']['name'];
+            $target_file   = $target_dir.basename($photo);
+            $uploadOk      = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+            // ------------- CODE TO VALIDATE IF THE IMAGE IS VALID -------------- //
+            // 1st CHECK TO SEE IF FILE EXIST ALREADY
+            if (file_exists($target_file)) {
+                $photoErr[] = "Sorry, file already exists <br>";
+                $uploadOk = 0;
+            }
+            // 2nd CHECK FILE SIZE
+            if ($_FILES["photo"]["size"] > 900000) {
+                $photoErr[] = "Sorry, your file is too large <br>";
+                $uploadOk = 0;
+            }
+            // 3rd CHECK TO SEE IF FILE FORMAT IS A jpg, jpeg, png, gif
+            if ($imageFileType != "jpg" 
+                && $imageFileType != "jpeg" 
+                && $imageFileType != "png"
+                && $imageFileType != "gif"
+            ) {
+                $photoErr[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed <br>";
+                $uploadOk = 0;
+            }
+            // ------------------------------------------------------------------- //
+            // ?4th STORE THE getimagesize() ARRAY DATA FROM THE $_FILE SUPER GLOBAL
+            // $verify_image = getimagesize($_FILES["photo"]["tmp_name"]);
+        
+            // ?5th CHECK THE STORED getimagesize() ARRAY IS A VALID IMAGE
+            // if (!$verify_image) {
+            //     $photoErr[] = "File is not an image <br>";
+            //     $uploadOk = 0;
+            // }
+            // ------------------------------------------------------------------- //
+            // 5th Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                $photoErr[] = "Sorry, your file was not uploaded <br>";
+            } else {
+                $movImg = move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
+                if (!$movImg) {
+                    $photoErr[] = "Sorry, there was an error uploading your image <br>";
+                }
+            }
+            // ---------- CODE TO INSERT VALIDATED FORM DATA TO DATABASE --------- //
+            $database      = new Database("localhost", "root", "root", "cms");
+        
+            $sql           = "INSERT INTO user_record(name, 
+                                                        email, 
+                                                        contact_num, 
+                                                        username, 
+                                                        password, 
+                                                        photo)
+                                VALUES(:namE, 
+                                        :emaiL, 
+                                        :contact_nuM, 
+                                        :usernamE, 
+                                        :passworD, 
+                                        :photO)";
+        
+            $prepare_stmt  = $database->conn()->prepare($sql);
+            $prepare_stmt->bindValue(':namE', $name);
+            $prepare_stmt->bindValue(':emaiL', $email);
+            $prepare_stmt->bindValue(':contact_nuM', $contact_num);
+            $prepare_stmt->bindValue(':usernamE', $username);
+            $prepare_stmt->bindValue(':passworD', $hashed_password);
+            $prepare_stmt->bindValue(':photO', $photo);
+            $execute = $prepare_stmt->execute();
+        
+            if ($execute) {
+                $test_db = null;
+                $_SESSION["success_message"] = "You have registered, please login";
+                redirect("login.php");
+        
+            } else {
+                $test_db = null;
+                $_SESSION["error_message"] = "Record has not been submitted";
+            }
+            // ------------------------------------------------------------------- //
+        }
+    }
+    // --------------------------- TEST CODE -------------------------- //
+    // echo "<pre>";
+    // print_r($verify_image);
+    // echo "</pre>";
+    // exit;
+    // ---------------------------------------------------------------- //
+    // VIEW THE IMAGE DETAILS IN THE $_FILE SUPER GLOBAL
+    // echo "<pre>";
+    // print_r($_FILES);
+    // echo "</pre>";
+    // exit;
+    // ---------------------------------------------------------------- //
+    // echo "<pre>";
+    // print_r($photoErr);
+    // echo "</pre>";
+    // exit;
+    // ---------------------------------------------------------------- //
+    // echo "<pre>";
+    // print_r($formDataErr);
+    // echo "</pre>";
+    // exit;
+    // ---------------------------------------------------------------- //
+
 }
 
 ?>
@@ -83,53 +274,58 @@ if (isset($_POST['submit'])) {
       <!---------------------- CLOSING HTML TAGS AND NAV LINKS --------------------->
 
       <div class="reg_form mt-5 mb-5">
+      <?php 
+        echo errorMessage(); 
+        echo successMessage(); 
+        ?>
+    <br>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  class="dbform" method="POST" enctype="multipart/form-data">
           <fieldset>
-            <span class="fieldinput">Name:</span>
+            <label for="name" class="fieldinput">Name:</label>
             <br>
-            <input type="text" name="name" value="
-            <?php if (isset($name)) {
-                echo $name;
-            } ?>">
+            <input type="text" id="name" name="name" value="<?php echo $name;?>">
+            <span class="error">* <?php echo $nameErr;?></span>
             <br>
-            <span class="fieldinput">Email:</span>
+
+            <label for="email" class="fieldinput">email:</label>
             <br>
-            <input type="text" name="email" value="
-            <?php if (isset($email)) {
-                echo $email;
-            } ?>">
+            <input type="text" id="email" name="email" value="<?php echo $email;?>">
+            <span class="error">* <?php echo $emailErr;?></span>
             <br>
-            <span class="fieldinput">Contact Number:</span>
+            <!---------------------------------------------------------------------->
+            <label for="contact_num" class="fieldinput">Number:</label>
             <br>
-            <input type="text" name="contact_num" value="
-            <?php if (isset($contact_num)) {
-                echo $contact_num;
-            } ?>">
+            <input type="text" id="contact_num" name="contact_num" value="<?php echo $contact_num;?>">
+            <span class="error">* <?php echo $contactErr;?></span>
             <br>
-            <span class="fieldinput">User Name:</span>
+            <!---------------------------------------------------------------------->
+            <label for="username" class="fieldinput">Username:</label>
             <br>
-            <input type="text" name="username" value="
-            <?php if (isset($username)) {
-                echo $username;
-            } ?>">
+            <input type="text" id="username" name="username" value="<?php echo $username;?>">
+            <span class="error">* <?php echo $usernameErr;?></span>
             <br>
-            <span class="fieldinput">Password:</span>
+            <!---------------------------------------------------------------------->
+            <label for="password" class="fieldinput">Password:</label>
             <br>
-            <input type="password" name="password" value="
-            <?php if (isset($password)) {
-                echo $password;
-            } ?>">
+            <input type="password" id="password" name="password" value="<?php echo $password;?>">
+            <span class="error">* <?php echo $passwordErr;?></span>
             <br>
+            <!---------------------------------------------------------------------->
+            <label for="confirm_pass" class="fieldinput">Confirm Password:</label>
             <br>
-            <span class="fieldinput">Add Photo:</span>
-            <br>
-            <input type="file" name="photo" value="
-            <?php if (isset($photo)) {
-                echo $photo;
-            } ?>">
+            <input type="password" id="confirm_pass" name="confirm_pass" value="<?php echo $con_password;?>">
+            <span class="error">* <?php echo $confpassErr ." &nbsp; ". $passNoMatchErr;?></span>
             <br>
             <br>
+            <!---------------------------------------------------------------------->
+            <label for="photo" class="fieldinput">Photo:</label>
+            <br>
+            <input type="file" id="photo" name="photo" value="">
+            <br>
+            <br>
+            <!---------------------------------------------------------------------->
             <input type="submit" name="submit" value="Submit Record:">
+            <!---------------------------------------------------------------------->
           </fieldset>
         </form>
       </div>
